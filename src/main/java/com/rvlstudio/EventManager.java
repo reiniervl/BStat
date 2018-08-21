747c71a3e663b0d81ca43daad8e02b8bd8d5d220
Reinier van Leussen <rvanleussen@outlook.com>
2018-08-21 21:02:55 +0200

    Manager update

--- src/main/java/com/rvlstudio/EventManager.java
+++ src/main/java/com/rvlstudio/EventManager.java
@@ -13,6 +13,21 @@ public class EventManager extends Observable {
 		EventManager.dao = SqlEventDAO.getDAO();
 		events = new ArrayList<>();
 	}
+
+	public void addEvent(Event<?> event) {
+		dao.addEvent(event);
+		notifyObservers("ADD");
+	}
+
+	public void deleteEvent(Event<?> event) {
+		dao.deleteEvent(event);
+		notifyObservers("DELETE");
+	}
+
+	public void updateEvent(Event<?> event) {
+		dao.updateEvent(event);
+		notifyObservers("UPDATE");
+	}
 	
 	public void setListToMonth() {
 		Calendar start = Calendar.getInstance();
@@ -31,6 +46,40 @@ public class EventManager extends Observable {
 		notifyObservers("MONTH");
 	}
 	
+	public void setListToWeek() {
+		Calendar start = Calendar.getInstance();
+		start.set(Calendar.DAY_OF_WEEK, 0);
+		start.set(Calendar.HOUR, 0);
+		start.set(Calendar.MINUTE, 0);
+		start.set(Calendar.SECOND, 0);
+
+		Calendar end = Calendar.getInstance();
+		end.set(Calendar.DAY_OF_WEEK, 7);
+		end.set(Calendar.HOUR, 23);
+		end.set(Calendar.MINUTE, 59);
+		end.set(Calendar.SECOND, 59);
+
+		events = new ArrayList<Event<?>>(dao.getEvents(start, end));
+		notifyObservers("WEEK");
+	}
+	
+	public void setListToWeek(Calendar week) {
+		Calendar start = (Calendar)week.clone();
+		start.set(Calendar.DAY_OF_WEEK, 0);
+		start.set(Calendar.HOUR, 0);
+		start.set(Calendar.MINUTE, 0);
+		start.set(Calendar.SECOND, 0);
+
+		Calendar end = week;
+		end.set(Calendar.DAY_OF_WEEK, 7);
+		end.set(Calendar.HOUR, 23);
+		end.set(Calendar.MINUTE, 59);
+		end.set(Calendar.SECOND, 59);
+
+		events = new ArrayList<Event<?>>(dao.getEvents(start, end));
+		notifyObservers("WEEK");
+	}
+
 	public void setListToDay() {
 		Calendar start = Calendar.getInstance();
 		start.set(Calendar.HOUR, 0);
